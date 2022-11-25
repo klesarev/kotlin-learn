280e99cfdec6088b2dfab7ab29b1095b87ddb9cf
klesarev <iklesarev@moneycat.asia>
2022-11-25 22:29:08 +0300

    refactor

--- src/main/kotlin/generics/GenericLearn.kt
+++ src/main/kotlin/generics/GenericLearn.kt
@@ -5,33 +5,17 @@ import kotlin.random.Random
 import kotlin.reflect.KProperty
 
 fun main(){
-    // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987,
-   println(
-       fibonacci(10).take(10).toList()
-   )
-}
 
-
-
-
-
-val factorial = generateSequence(1) {
-    it * (it + 1)
 }
 
 val numbers = listOf<Int>(16,21,28)
 val names = listOf<String>("Sara", "Demi", "Julia")
 
-interface Fullname {
-    fun getname(): String
-}
-
-
-class TestUser: Fullname {
+class TestUser {
     val age by Rand(numbers)
     val name by Rand(names)
-
-    override fun getname() = "$name, is $age"
+    val fullname: String
+        get() = "$name, is $age"
 }
 
 class Rand<V>(private val list: List<V>) {
