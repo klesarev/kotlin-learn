501ba026d15b5d6ab83ed165e149971bc207f6d5
klesarev <iklesarev@moneycat.asia>
2022-11-27 02:00:52 +0300

    add fibonacci func's

--- src/main/kotlin/algorithms/Fibonacci.kt
+++ src/main/kotlin/algorithms/Fibonacci.kt
@@ -1,7 +1,15 @@
 package android
 
-fun main(){
+import kotlin.math.sqrt
 
+fun main(){
+    // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+    val digit = 10
+    run {
+        println("Fibonacci - Sequence($digit) is ${fibonacci(digit).toList().last()}")
+        println("Fibonacci - Simple($digit) is ${fibSimple(digit)}")
+        println("Fibonacci - Array($digit) is ${fibArray(digit)}")
+    }
 }
 
 fun fibonacci(num: Int = 10): Sequence<Int> {
@@ -10,13 +18,7 @@ fun fibonacci(num: Int = 10): Sequence<Int> {
     }.map { it.first }.take(num)
 }
 
-val fibSeq: (num: Int) -> Sequence<Pair<Int, Int>> = {
-    generateSequence (Pair(0,1)) {
-        Pair(it.second, it.first + it.second)
-    }
-}
-
-fun fib(num: Int): Int {
+fun fibSimple(num: Int): Int {
     var start = Pair(0,1)
     val list = mutableListOf<Pair<Int, Int>>()
     list.add(start)
@@ -27,3 +29,19 @@ fun fib(num: Int): Int {
     return list.last().first
 }
 
+fun fibArray(n: Int): Int {
+
+    // массив с будующими числами
+    // fb[0] - первый индекс, fb[1] - второй индекс
+    val fb: IntArray = IntArray(n)
+    fb[0] = 0
+    fb[1] = 1
+
+    // строим последовательность
+    (2 until n).forEach { index->
+        fb[index] = fb[index - 1] + fb[index - 2]
+    }
+
+    // возвращаем искомый элемент
+    return fb[n-1]
+}
