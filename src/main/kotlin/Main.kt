5140a8adcbcbc6a8b5b2b442cfcae4aad954131c
klesarev <iklesarev@moneycat.asia>
2022-11-27 00:45:37 +0300

    add interview questions: part 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,45 +3,19 @@ import kotlin.random.Random
 import kotlin.reflect.KProperty
 
 fun main(args: Array<String>) {
-
+    println(
+        fib().take(10).toList().last().second
+    )
 }
 
-val intlist = listOf<Int>(16,21,28)
-class TestUser() {
-    val name by Rand(intlist)
-}
-class Rand<V>(private vararg val list: V) {
-    operator fun <T> getValue(thisRef: T, property: KProperty<*>): V {
-        return list[Random.nextInt(0,list.size)]
-    }
+fun fizzbuzz(n: Int): Any = when {
+    n % 15 == 0 -> "FizzBuzz"
+    n % 3 == 0 -> "Fizz"
+    n % 5 == 0 -> "Buzz"
+    else -> n
 }
 
-
-
-
-sealed class City2(val id: Int) {
-    class Donetsk: City2(1)
-    class Moscow: City2(2)
-    class Lissabon(val favor: Boolean): City2(3)
+fun fib() = generateSequence(Pair(0,1)) {
+    Pair(it.second, it.first + it.second)
 }
-sealed class MetaTag (private val pattern: String) {
-    class Title: MetaTag("<title.*?>(.*?)</title>")
-    class Description: MetaTag("<meta name=\"description\" content=(.*?)/>")
-
-    fun get(data: String): String {
-        val res = Pattern.compile(pattern).matcher(data)
-        return if (res.find()) res.group(1) else "${this::class.simpleName} not found"
-    }
-}
-
-fun countC(text: String?) {
-    text?.let {
-        println(it.length)
-    } ?: println("null!!!")
-}
-
-fun countC2(text: String?) {
-    println(text?.length ?: "NULL")
-}
-
 
